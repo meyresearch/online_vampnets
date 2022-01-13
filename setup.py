2a1d6b4adf312e27e2519e0324e3fc9765357071
Robert Arbon <robert.arbon@gmail.com>
2022-01-13 20:29:49 +0000

    started entry point

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+setuptools.setup(
+    name="celerity",
+    version="0.1",
+    description="Creates deep MSMs using accelerated training methods",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "addict",
+        "click",
+        "devtools",
+        "mdtraj",
+        "numpy",
+        "deeptime",
+        "h5py",
+        "mdshare",
+        "pretty-errors",
+        "pandas",
+        "tables",
+        "matplotlib"
+    ],
+    entry_points={
+        'console_scripts': [
+            'celer = celerity.main:cli',
+        ],
+    },
+    python_requires=">=3.8",
+)
